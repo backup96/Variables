52319a353a0d0b2bd6fb1ecc3d2e08de5dda589a
backup96 <109244276+backup96@users.noreply.github.com>
2024-07-13 10:37:46 -0500

    Las variables de name, birthday y lastName fueron creadas para mostrar ejemplos de uso de variabls en Kotlin

---
+++ Variables.kt
@@ -0,0 +1,7 @@
+var name = "John" // Variables que pueden ser defninidas más de unas vez
+
+val birthday = "5 de marzo 1970" // Variables que solo pueden ser defninidas una sola vez
+
+birthday = "6 de abril 1975" // Esto lanzará un error
+
+var lastName: String = "Frusciante"
