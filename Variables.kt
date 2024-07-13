fd3aae4475036bf501085cc9d50ac3cfc8929af2
backup96 <109244276+backup96@users.noreply.github.com>
2024-07-13 10:55:47 -0500

    Se cambio la descripción de cada ejemplo y se agrego la variable guitar para ejemplificar otro aspecto de las variables en Kotlin

--- Variables.kt
+++ Variables.kt
@@ -1,7 +1,13 @@
 var name = "John" // Variables que pueden ser defninidas más de unas vez
 
+name = "Antony"
+
+var lastName: String = "Frusciante" // No es necesario colocar el tipo de dato de la variable
+
 val birthday = "5 de marzo 1970" // Variables que solo pueden ser defninidas una sola vez
 
 birthday = "6 de abril 1975" // Esto lanzará un error
 
-var lastName: String = "Frusciante"
+val guitar: String // Una variable puede ser creada primero
+
+guitar = "Fender" // Y despues inicializada
